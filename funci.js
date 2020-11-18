c5e0881feb55f8afc66b8cc96cb2605ea1357bff
isabelast123 <72816987+isabelast123@users.noreply.github.com>
2020-11-17 22:55:37 -0500

    Add files via upload

---
+++ funci.js
@@ -0,0 +1,27 @@
+var click1=function()
+{
+	n=document.getElementById('hola1');
+	n.className="hola11";
+	boton=document.getElementById('boton1');
+	boton.style.display="none";
+	setTimeout(pasa,);
+}
+var click2=function()
+{
+	n=document.getElementById('hola2');
+	n.className="hola22";
+	boton=document.getElementById('boton2');
+	boton.style.display="none";
+	window.location.href='personaje1.html';
+
+}
+
+
+
+
+var pasa=function()
+{
+	window.location.href='pagina1.html';
+	setTimeout(audiio,100);
+}
+
